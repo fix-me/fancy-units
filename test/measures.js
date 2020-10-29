1a3cd88a14da5ced586a09fbc0329e2e963bc9c7
fix-me <43244072+fix-me@users.noreply.github.com>
2020-10-29 17:11:41 +0100

    Release 2.3.15
    import updated code, do release

---
+++ test/measures.js
@@ -0,0 +1,38 @@
+var convert = require("../lib"),
+  assert = require("assert"),
+  tests = {};
+
+tests["measures"] = function () {
+  var actual = convert().measures(),
+    expected = [
+      "length",
+      "area",
+      "mass",
+      "volume",
+      "each",
+      "temperature",
+      "time",
+      "digital",
+      "partsPer",
+      "speed",
+      "pace",
+      "pressure",
+      "current",
+      "voltage",
+      "power",
+      "reactivePower",
+      "apparentPower",
+      "energy",
+      "reactiveEnergy",
+      "volumeFlowRate",
+      "illuminance",
+      "frequency",
+      "angle",
+      "charge",
+      "force",
+      "acceleration",
+    ];
+  assert.deepEqual(actual, expected);
+};
+
+module.exports = tests;
